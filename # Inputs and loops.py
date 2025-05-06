9295582c7e82ffc8f8aea88e180e853948052366
chux4592 <ernest.wang@kingsway.school.nz>
2025-05-07 10:56:47 +1200

    Add files via upload
    Demonstrated how to get an input from a user and store it

---
+++ # Inputs and loops.py
@@ -0,0 +1,22 @@
+# Inputs and loops
+# Authour: Ernest Wang
+# Date: 07/05/2025
+# Version 1.0
+
+# Ask the user a question and store their answer in a variable
+# Ask the user for their name and store it 
+name = input("Kia ora,  What is your name?")
+
+# Ask the user for two numbers and then add them together.
+num1 = input("What is your first number?")
+num2 = inout('What is your second numnber?')
+print(f"You entered your first number as {num1}")
+print(f"You entered your second number as {num2}")
+
+# Adding the two answers together 
+sum = num1 +num2
+print(sum)
+
+# Test that input was stored correctly
+print(name)
+# To comment code out, use ctrl + /
