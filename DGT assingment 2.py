f98deafe564a7e41ab399ac1ae1ab532244b9f7b
chux4592 <ernest.wang@kingsway.school.nz>
2025-06-13 16:57:29 +1200

    Add files via upload

--- DGT assingment 2.py
+++ DGT assingment 2.py
@@ -13,13 +13,14 @@ def calculator():
             if cost_per_metre <= 0:
                 print("Cost per metre must be greater than 0.")
                 continue
-            perimeter = 2*(length*width)
+            perimeter = 2*(length + width)
 
             total_cost = perimeter * cost_per_metre
             print(f"The total cost to fence your {perimeter}m rectangular area is {total_cost}")
 
             user_choice = input("Do you have another calculation in mind? ")
-            if user_choice !="":
+            if user_choice !="yes":
+                
                 print("Exiting, Goodbye!")
                 break # Exit loop after successful calculation
 
